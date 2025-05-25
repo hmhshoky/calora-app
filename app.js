28a2daed574b3cc7ea02d725d22bfa89211b5384
ArtonTroni <troniarton@gmail.com>
2025-05-25 20:38:13 +0200

    läuft

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', exphbs.create().engine);
+// Static files
+app.use(express.static('public_html'));
+
+// View engine setup
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('public_html'));
-
-// Statische Dateien aus public_html
-app.use(express.static('public_html'));
-
-// Startseite
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public_html', `counter.html`));
+  res.render('counter');
 });
 
-// Redirect für /maintain → /maintain.html
 app.get('/maintain', (req, res) => {
-  res.redirect('/maintain.html');
+  res.render('maintain');
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}`);
-});
-
-app.get('/meals', (req, res) => {
-  const meals = [
-    { name: 'Spaghetti', kcal: 500 },
-    { name: 'Salad', kcal: 150 },
-    { name: 'Pizza', kcal: 800 }
-  ];
-
-  res.render('meals', { meals });
 });
